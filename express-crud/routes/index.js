ba35ac985c343bdf9d900f5cf7896a05c278053a
joonayd <63727069+joonayd@users.noreply.github.com>
2021-06-25 15:24:28 +0500

    Update index.js

--- express-crud/routes/index.js
+++ express-crud/routes/index.js
@@ -1,16 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.get('/cart', function(req, res, next) {
-  let cart = req.cookies.cart
-  if(!cart) cart = []
+router.get("/cart", function (req, res, next) {
+  let cart = req.cookies.cart;
+  if (!cart) cart = [];
 
-  res.render("cart",{cart})
+  res.render("cart", { cart });
 });
 
 module.exports = router;
